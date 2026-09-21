9529e4b7fb3be40e86e0dd54aef5cb637a1f9285
muskankumari98 <muskansoni6207@gmail.com>
2026-09-21 20:26:10 +0530

    add previous Java DSA practice codes

---
+++ dowhile.java
@@ -0,0 +1,11 @@
+public class dowhile {
+    public static void main(String args[]){
+        int i=23;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<11);
+
+    }
+}
---
+++ tableloop.java
@@ -0,0 +1,9 @@
+public class tableloop {
+public static void main(String args[]){
+     int n=2;
+for(int i=1;i<=10;i++){
+    System.out.println(i*n);
+
+}
+}
+}
